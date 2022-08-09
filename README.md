502bc9308747fb5219831c431405f1c5f6086838
Aleksandr Dis <a.dis@awatera.com>
2022-08-09 21:39:08 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,40 @@
+# Pure JS tricks
+
+## Table of Contents
+
+- [About](#about)
+- [Photo slider](#photoslider)
+- [Usage](#usage)
+- [Contributing](../CONTRIBUTING.md)
+
+## About <a name = "about"></a>
+
+This is project with many pure JS tricks which should be in arsenal of every developer!
+
+## Photo slider <a name = "photoslider"></a>
+
+Pure photo slider with select animation
+
+<img src="./photo-slider-js/gif/ezgif.com-gif-maker.gif">
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running.
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo.
+
+## Usage <a name = "usage"></a>
+
+Add notes about how to use the system.
--- photo-slider-js/index.js
+++ photo-slider-js/index.js
@@ -1,25 +1,34 @@
-const slides = document.querySelectorAll(".slide");
-const descriptions = document.querySelectorAll(".description");
+function sliderPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll(".slide");
+  const descriptions = document.querySelectorAll(".description");
 
-slides.forEach((slide) => {
-  slide.addEventListener("click", () => {
-    const classList = Object.values(slide.classList);
-    if (classList.some((currentClass) => currentClass === "active")) {
-      unsetActiveAllSlides();
-      unsetActiveAllDescription();
-    } else {
-      unsetActiveAllSlides();
-      unsetActiveAllDescription();
-      slide.classList.add("active");
-      slide.children[1].classList.add("active");
-    }
+  slides[activeSlide].classList.add("active");
+  descriptions[activeSlide].classList.add("active");
+
+  slides.forEach((slide) => {
+    slide.addEventListener("click", () => {
+      const classList = Object.values(slide.classList);
+      if (classList.some((currentClass) => currentClass === "active")) {
+        unsetActiveAllSlides();
+        unsetActiveAllDescription();
+      } else {
+        unsetActiveAllSlides();
+        unsetActiveAllDescription();
+        slide.classList.add("active");
+        slide.children[1].classList.add("active");
+      }
+    });
   });
-});
 
-const unsetActiveAllSlides = () => {
-  slides.forEach((slide) => slide.classList.remove("active"));
-};
+  const unsetActiveAllSlides = () => {
+    slides.forEach((slide) => slide.classList.remove("active"));
+  };
 
-const unsetActiveAllDescription = () => {
-  descriptions.forEach((description) => description.classList.remove("active"));
-};
+  const unsetActiveAllDescription = () => {
+    descriptions.forEach((description) =>
+      description.classList.remove("active")
+    );
+  };
+}
+
+sliderPlugin();
